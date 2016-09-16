39aff8c97c3e9c735e24402b63a8d326caf77b2b
Brian Gamble <cc@theclearcoder.com>
2016-09-16 12:48:30 -0500

    Exercise 1: Saying Hello

---
+++ lib/hello.ex
@@ -0,0 +1,19 @@
+defmodule Hello do
+  def greet do
+    get_name
+    |> cleanup
+    |> display_greeting
+  end
+
+  def get_name do
+    IO.gets "What is your name? "
+  end
+
+  def cleanup(str) do
+    String.trim(str)
+  end
+
+  def display_greeting(name) do
+    IO.puts "Hello, #{name}, nice to meet you!"
+  end
+end
---
+++ test/hello_test.exs
@@ -0,0 +1,21 @@
+defmodule HelloTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "prompts the user for a name" do
+    assert capture_io(fn ->
+      Hello.get_name
+    end) == "What is your name? "
+  end
+
+  test "removes spaces and newlines from input" do
+    assert Hello.cleanup("Bob\n") == "Bob"
+    assert Hello.cleanup(" Bob ") == "Bob"
+  end
+
+  test "display a message to user using the given name" do
+    assert capture_io([input: "Frank\n", capture_prompt: false], fn ->
+      Hello.greet
+    end) == "Hello, Frank, nice to meet you!\n"
+  end
+end
